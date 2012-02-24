542ac1fe449679068d5e3ecb352dfa905f1fe30c
Juha V Miettinen <juha.v.miettinen@helsinki.fi>
2012-02-24 17:52:32 +0200

    Chromosome names are now clickable and highlighted in red.

---
+++ src/fi/csc/microarray/client/visualisation/methods/gbrowserng/model/ChromoName.java
@@ -0,0 +1,48 @@
+package fi.csc.microarray.client.visualisation.methods.gbrowserng.model;
+
+import fi.csc.microarray.client.visualisation.methods.gbrowserng.data.Chromosome;
+import math.Vector2;
+
+public class ChromoName {
+	private Chromosome chromosome;
+	private Vector2 position = new Vector2();
+	private Vector2 size = new Vector2();
+	private boolean active = false;
+	
+	public ChromoName(Chromosome chromo) {
+		this.chromosome = chromo;
+	}
+	
+	public boolean isOver(float x, float y) {
+		return active = (position.x - size.x/2 < x
+				&& position.x + size.x/2 > x
+				&& position.y - size.y/2 < y
+				&& position.y + size.y/2 > y);
+	}
+	
+	public Chromosome getChromosome() {
+		return chromosome;
+	}
+	
+	public void setPosition(float x, float y) {
+		position.x = x;
+		position.y = y;
+	}
+	
+	public Vector2 getPosition() {
+		return new Vector2(position);
+	}
+	
+	public Vector2 getSize() {
+		return new Vector2(size);
+	}
+
+	public void setSize(float w, float h) {
+		size.x = w;
+		size.y = h;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+}
