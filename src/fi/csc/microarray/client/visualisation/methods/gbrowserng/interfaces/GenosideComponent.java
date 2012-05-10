d3022246a26fb3ca01adcd1fe475f80d6ddc0288
Mammutti <Mammutti@Mammoth>
2012-05-11 00:03:36 +0200

    javadoc for GenoSideComponent (1 line - useful or what!)

--- src/fi/csc/microarray/client/visualisation/methods/gbrowserng/interfaces/GenosideComponent.java
+++ src/fi/csc/microarray/client/visualisation/methods/gbrowserng/interfaces/GenosideComponent.java
@@ -4,6 +4,10 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import javax.media.opengl.GL2;
 
+/**
+ * Superclass for Overview window - the main user interface.
+ * Specifies functionality for Overview to implement, based on which it can be drawn using OpenGL.
+ */
 public abstract class GenosideComponent {
 
 	public abstract boolean handle(MouseEvent event, float screen_x, float screen_y);
