a5096b22abb4bdf9b2dfebdd066a068f102e0acc
Mammutti <Mammutti@Mammoth>
2012-05-11 11:07:40 +0200

    javadoc for ChromoName

--- src/fi/csc/microarray/client/visualisation/methods/gbrowserng/model/ChromoName.java
+++ src/fi/csc/microarray/client/visualisation/methods/gbrowserng/model/ChromoName.java
@@ -3,16 +3,29 @@ package fi.csc.microarray.client.visualisation.methods.gbrowserng.model;
 import com.soulaim.tech.math.Vector2;
 import fi.csc.microarray.client.visualisation.methods.gbrowserng.data.ViewChromosome;
 
+/**
+ * Class represents the name of each chromome as displayed on screen outside circle.
+ */
 public class ChromoName {
 	private ViewChromosome chromosome;
 	private Vector2 position = new Vector2();
 	private Vector2 size = new Vector2();
 	private boolean active = false;
 	
+	/**
+	 * Creates a new ChromoName, linked to a cerain ViewChromosome
+	 * @param chromo The chromosome
+	 */
 	public ChromoName(ViewChromosome chromo) {
 		this.chromosome = chromo;
 	}
 	
+	/**
+	 * Returns knowledge of whether mouse cursor is hovering over the ChromoName
+	 * @param x vertical location of mouse
+	 * @param y horizontal location of mouse
+	 * @return whether mouse cursor is hovering over the ChromoName
+	 */
 	public boolean isOver(float x, float y) {
 		return active = (position.x - size.x/2 < x
 				&& position.x + size.x/2 > x
